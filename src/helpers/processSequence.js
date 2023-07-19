d1272cdb73b15aba6e09a9af89acf816e7e24039
Vlad <vla.koncha@gmail.com>
2023-07-19 15:53:03 +0700

    feat: implements writeLog and validation

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,77 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from "../tools/api";
+import {
+  __,
+  allPass,
+  compose,
+  gt,
+  ifElse,
+  length,
+  lt,
+  partial,
+  tap,
+  test,
+} from "ramda";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+/**
+ * Я – пример, удали меня
+ */
+const wait = (time) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const numberOfCharsLessThanTen = compose(lt(__, 10), length);
+const numberOfCharsMoreThanTwo = compose(gt(__, 2), length);
+const numberMustBePositive = compose(gt(__, 0), Number);
+const numberRegex = /^[0-9]+(\.[0-9]+)?$/;
+const isValueValidNumber = test(numberRegex);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const isValidValue = allPass([
+  numberOfCharsMoreThanTwo,
+  numberOfCharsLessThanTen,
+  numberMustBePositive,
+  isValueValidNumber,
+]);
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const handleValidationError = partial(handleError, ["ValidationError"]);
+  const writeLogValue = tap(writeLog);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+  const sequence = writeLogValue;
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+  const runSequence = ifElse(isValidValue, sequence, handleValidationError);
+  const writeLogAndRunSequence = compose(runSequence, writeLogValue);
+  writeLogAndRunSequence(value);
+
+  api
+    .get("https://api.tech/numbers/base", {
+      from: 2,
+      to: 10,
+      number: "01011010101",
+    })
+    .then(({ result }) => {
+      writeLog(result);
+    });
+
+  wait(2500)
+    .then(() => {
+      writeLog("SecondLog");
+
+      return wait(1500);
+    })
+    .then(() => {
+      writeLog("ThirdLog");
+
+      return wait(400);
+    })
+    .then(() => {
+      handleSuccess("Done");
+    });
+};
 
 export default processSequence;
